1c2f19a8f18afbed3749c8681b76a730e2e97741
Pravin Patel <patelpra337@gmail.com>
2019-11-16 14:10:15 -0600

    Setup storyboards and view controllers

---
+++ Grail Diary/Grail Diary/ViewControllers/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by patelpra on 11/16/19.
+//  Copyright © 2019 Crus Technologies. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
